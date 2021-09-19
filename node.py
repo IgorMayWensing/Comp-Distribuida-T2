a4785dac37b6a7ecea61ef331f27b92046e3acf3
Igor May Wensing <36992828+IgorMayWensing@users.noreply.github.com>
2021-09-19 19:52:16 -0300

    Final commit
    Funfando coisa linda

--- node.py
+++ node.py
@@ -1,10 +1,13 @@
 from threading import Thread
+from datetime import datetime
 import random
 import socket
 import time
 import sys
 
 isLeader = False
+leader = -1
+isParticipant = False
 pid = int(sys.argv[1])
 print('Processo PID: ' + sys.argv[1])
 dest = ('localhost', (pid + 1) % 5 + 9000)
@@ -12,11 +15,6 @@ r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 r.bind(('0.0.0.0', pid))
 r.listen(1)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-if len(sys.argv) >= 3:
-    if sys.argv[2] == 'lider':
-        isLeader = True
-        print('Sou o primeiro LÍDER')
 
 
 def find_node():
@@ -27,18 +25,22 @@ def find_node():
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect(dest)
+            print("Anel restaurado")
             break
         except ConnectionRefusedError:
             if attempt == 0:
                 attempt = 3
                 dest = ('localhost', (dest[1] + 1) % 5 + 9000)
             else:
-                time.sleep(5)
+                time.sleep(1)
                 attempt -= 1
 
 
 def listen():
     global dest
+    global isLeader
+    global isParticipant
+    global leader
     while True:
         con, _ = r.accept()
         while True:
@@ -47,29 +49,92 @@ def listen():
                 con.close()
                 break
             res = data.decode('UTF-8')
-            print(res)
             op = res.split(' ')
+            if op[0] != 'hc':
+                print('Mensagem recebida: ' + res)
             if op[0] == 'hl':
                 if not isLeader:
                     if str(pid) != op[1]:
                         try:
                             s.sendall(data)
-                            print("PROPAGUEI")
                         except BrokenPipeError:
                             s.close()
                             find_node()
                     else:
-                        print("VOTO IMPRESSO JÁ!")
+                        print("MEU DEUS, O LÍDER MORREU!")
                         # eleição
+                        isParticipant = True
+                        try:
+                            s.sendall(('el '+ str(pid) + ' ' + str(pid)).encode('UTF-8'))
+                            print("Nova eleição iniciada")
+                        except BrokenPipeError:
+                            s.close()
+                            find_node()
+            elif op[0] == 'el':  
+                if op[2] == str(pid):
+                    leader = int(op[1])
+                    isParticipant = False
+                    if leader == pid:
+                        isLeader = True
+                    try:
+                        s.sendall(('r2 '+ op[1] + ' ' + str(pid)).encode('UTF-8'))
+                        print("Iniciando round 2")
+                        print('----------------------')
+                        print('Leader: ' + str(leader))
+                        print('isLeader: ' + str(isLeader))
+                        print('isParticipant:' + str(isParticipant))
+                        print('----------------------')
+                    except BrokenPipeError:
+                        s.close()
+                        find_node()                           
+                elif op[1] > str(pid):
+                    isParticipant = True
+                    try:
+                        s.sendall(data)
+                        print("Definindo novo potencial líder")
+                    except BrokenPipeError:
+                        s.close()
+                        find_node()
+                elif op[1] < str(pid) and not isParticipant:
+                    isParticipant = True
+                    try:
+                        s.sendall(('el '+ str(pid) + ' ' + op[2]).encode('UTF-8'))
+                        print("Propagando pontencial líder")
+                    except BrokenPipeError:
+                        s.close()
+                        find_node()
+                
+            elif op[0] == 'r2' and op[2] !=  str(pid):
+                    leader = int(op[1])
+                    isParticipant = False
+                    if leader == pid:
+                        isLeader = True
+                    try:
+                        s.sendall(data)
+                        print('----------------------')
+                        print('Leader: ' + str(leader))
+                        print('isLeader :' + str(isLeader))
+                        print('isParticipant :' + str(isParticipant))
+                        print('----------------------')
+                    except BrokenPipeError:
+                        s.close()
+                        find_node()
 
 
 def send_periodically():
     while True:
-        time.sleep(10)
-        if random.random() < 0.10:
+        time.sleep(5)
+        random.seed(datetime.now())
+        if not isLeader and random.random() < 0.10:
             try:
                 s.sendall(('hl ' + str(pid)).encode('UTF-8'))
-                print("ENVIEI")
+                print("Procurando lider...")
+            except BrokenPipeError:
+                s.close()
+                find_node()
+        else:
+            try:
+                s.sendall('hc'.encode('UTF-8'))
             except BrokenPipeError:
                 s.close()
                 find_node()
--- start.py
+++ start.py
@@ -2,6 +2,6 @@ import os
 
 os.system("gnome-terminal --geometry=50x20+460+0 -- bash -c \'python3 node.py 9000; exec bash\'")
 os.system("gnome-terminal --geometry=50x20+920+0 -- bash -c \'python3 node.py 9001; exec bash\'")
-os.system("gnome-terminal --geometry=106x10+460+410 -- bash -c \'python3 node.py 9002 lider; exec bash\'")
+os.system("gnome-terminal --geometry=106x10+460+410 -- bash -c \'python3 node.py 9002; exec bash\'")
 os.system("gnome-terminal --geometry=50x20+460+900 -- bash -c \'python3 node.py 9003; exec bash\'")
 os.system("gnome-terminal --geometry=50x20+920+900 -- bash -c \'python3 node.py 9004; exec bash\'")
